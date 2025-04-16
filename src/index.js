68e8d8aaf45c5bf7b54492c0102f927460dcf4f1
PGGopika <pggopika369@gmail.com>
2025-04-17 03:52:16 +0530

    Fix: Ensure react-scripts is in dependencies

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css'; // If you have custom styles
@@ -8,4 +9,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+);  
